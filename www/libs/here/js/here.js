e3aca36b3467886372a95b53dcbab4b068d7385e
lingtalfi <lingtalfi@gmail.com>
2016-01-28 13:27:20 +0100

    initial commit

---
+++ www/libs/here/js/here.js
@@ -0,0 +1,214 @@
+(function ($) {
+
+
+    var pluginName = "here";
+
+
+    $[pluginName] = function (element, options) {
+
+        var defaults = {
+            //------------------------------------------------------------------------------/
+            // REQUIRED
+            //------------------------------------------------------------------------------/
+            /**
+             * The jquery selector for your events.
+             * This is required.
+             *
+             * Note that you are responsible for creating the html of the events.
+             * The target events must have the following attributes set:
+             *
+             * - data-duration: the duration of the events in second
+             * - data-offset: when does the event start ?
+             *                              relatively from the timeline's origin, in seconds.
+             *
+             *
+             */
+            jEvents: null,
+            //------------------------------------------------------------------------------/
+            // OPTIONAL
+            //------------------------------------------------------------------------------/
+            /**
+             * jquery handle to the outer container.
+             * The outer container is the html element that contains the inner container.
+             * The inner container is the html elements that contains the events.
+             * The inner container is moved inside of the outer container, and the outer container
+             * acts as a window to the inner container, that's how the timeline "moves" upon certain
+             * user actions.
+             * See docs for more details.
+             *
+             * Defaults to the direct parent of the html element to which the plugin was applied.
+             *
+             */
+            jOuterContainer: null,
+            /**
+             * The object responsible to handle the rendering of the time plots.
+             * A time plot plugin must have the following methods:
+             *
+             * - init ( hereInstance )
+             *          prepare the plugin if needed.
+             *          The here instance is passed so that one can attach methods to its prototype.
+             * - refresh ( ratio )
+             *          render the time plots according to the given ratio
+             *
+             *
+             */
+            timePlotPlugin: null,
+            /**
+             * How many pixels to represent 1 second.
+             * This is a number (it can be decimal, or just int), it can be bigger,
+             * equals to or lower than 1,
+             * but it must be strictly bigjPlotContainerger than 0.
+             */
+            ratio: 1,
+            /**
+             * The number of milliseconds to execute the moveTo animation.
+             * Default is 1000
+             */
+            moveToAnimationDuration: 1000,
+            /**
+             * The number of seconds that the whole timeline lasts.
+             * Default is 86400 (one day)
+             */
+            timelineDuration: 86400,
+            /**
+             * Callback fired after that an event is refreshed.
+             * Use this to set a background color dynamically, using a custom data-color attribute for instance,
+             * or to hide/show some elements depending on the new width, or...
+             *
+             * - jHandle: the jquery object representing the event
+             * - newWidth: int
+             * - data: the result of the jquery's data method, so you basically get any data- attribute's value.
+             *                  See jquery docs for more info.
+             *
+             *
+             */
+            onEventRefreshedAfter: function (jHandle, newWidth, data) {
+            },
+            /**
+             * Where do you want to start with, in seconds, and from the timeline's origin
+             */
+            startAt: 0
+        };
+
+        var plugin = this;
+
+        plugin.settings = {};
+        var $el = $(element);
+
+        /**
+         * some first class citizens vars inside this plugin
+         */
+        var ratio, jEvents, jOuterContainer;
+        var timePlotter = null;
+
+
+        plugin.init = function () {
+
+
+            plugin.settings = $.extend({}, defaults, options);
+            ratio = plugin.settings.ratio;
+            jEvents = plugin.settings.jEvents;
+
+
+
+
+            // resolving some default values
+            if (null === plugin.settings.jOuterContainer) {
+                plugin.settings.jOuterContainer = $el.parent();
+            }
+            jOuterContainer = plugin.settings.jOuterContainer;
+
+            if (null !== plugin.settings.timePlotPlugin) {
+                timePlotter = plugin.settings.timePlotPlugin;
+                timePlotter.init(plugin);
+            }
+            
+            
+            $el.css({left: '-' + secondsToPixels(plugin.settings.startAt) + 'px'});
+            refresh();
+        };
+
+
+        /**
+         * Offset is the number of seconds since the
+         * origin of the timeline.
+         */
+        plugin.moveTo = function (offset) {
+            $el.animate({
+                left: '-' + secondsToPixels(offset) + 'px'
+            }, plugin.settings.moveToAnimationDuration);
+        };
+
+        /**
+         * This method basically just assign a new ratio.
+         * The ratio is the number of pixels that you use to represent a second.
+         */
+        plugin.zoom = function (newRatio) {
+            ratio = newRatio;
+            refresh();
+        };
+
+
+        plugin.setRatio = function (newRatio) {
+            ratio = newRatio;
+        };
+        plugin.getRatio = function () {
+            return ratio;
+        };
+
+
+        var secondsToPixels = function (nbSeconds) {
+            return parseInt(nbSeconds) * ratio;
+        };
+
+
+        var refresh = function () {
+
+            var jParent = null;
+            var z = 0;
+            // refresh events
+            jEvents.each(function () {
+                var data = $(this).data();
+                var duration = data['duration'];
+                var offset = data['offset'];
+                var theWidth = parseInt(parseInt(duration) * ratio);
+                $(this).width(theWidth);
+                $(this).css({
+                    left: secondsToPixels(offset) + 'px',
+                    "z-index": z++
+                });
+                plugin.settings.onEventRefreshedAfter($(this), theWidth, data);
+                if (null === jParent) {
+                    jParent = $(this).parent(); // assuming there is only one parent for all events
+                }
+            });
+
+
+            // resize the inner container's width
+            var innerContainerWidth = secondsToPixels(plugin.settings.timelineDuration);
+            jParent.width(innerContainerWidth);
+
+
+            // refresh plots
+            if (null !== timePlotter) {
+                timePlotter.refresh(ratio);
+            }
+        };
+
+        plugin.init();
+
+    };
+
+
+
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (undefined == $(this).data(pluginName)) {
+                var plugin = new $[pluginName](this, options);
+                $(this).data(pluginName, plugin);
+            }
+        });
+
+    };
+
+})(jQuery);
